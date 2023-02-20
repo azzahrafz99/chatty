05e18db33d71e0d24b2e7fce0e537193d9262abb
Fatimah Az Zahra <azzahrafz@gmail.com>
2023-02-20 11:17:55 +0700

    Add stimulus and turbo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,14 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers"
 import './vendor';
 import UserForm from './components/user_form';
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 $(function() {
