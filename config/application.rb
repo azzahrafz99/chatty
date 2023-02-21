9359111878d21753a10d504834985f6e06ae57ee
Fatimah Az Zahra <azzahrafz@gmail.com>
2023-02-21 15:50:08 +0700

    Set timezone

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Chatty
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Jakarta'
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_record.default_timezone = :local
   end
 end
