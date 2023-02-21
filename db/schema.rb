2b1475cd1b44ff820bbcaad9c7614bac87df41a2
Fatimah Az Zahra <azzahrafz@gmail.com>
2023-02-21 17:54:19 +0700

    Add column status to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_101349) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_102756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_101349) do
     t.string "first_name"
     t.string "last_name"
     t.date "birthdate"
+    t.integer "status", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
