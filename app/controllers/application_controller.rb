c49fee181a8a370b1ab4e8b8dc2a29deb73f3faa
Fatimah Az Zahra <azzahrafz@gmail.com>
2023-02-22 09:52:20 +0700

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_status
-    current_user&.update!(status: User.statuses[:online]) if current_user
+    current_user&.update!(status: User.statuses[:online])
   end
 end
