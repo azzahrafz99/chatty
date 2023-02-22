8ad1efba9af5fe906f8b04487f89dc476ca01476
Fatimah Az Zahra <azzahrafz@gmail.com>
2023-02-22 09:52:44 +0700

    Fix message display time

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,4 +12,14 @@ class Message < ApplicationRecord
     is_participant = Participant.where(user_id: user.id, room_id: room.id).first
     throw :abort unless is_participant
   end
+
+  def today?
+    created_at.to_date == Date.current
+  end
+
+  def display_time
+    return created_at.strftime('%H:%M') if today?
+
+    created_at.strftime('%A %H:%M')
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Message do
     it { is_expected.to belong_to(:room) }
     it { is_expected.to belong_to(:user) }
   end
+
+  describe '#display_time' do
+    let(:today) { Date.current.strftime('%Y-%m-%d 09:00') }
+
+    let!(:message) do
+      create(:message, body: 'Hi', created_at: Time.zone.parse(today))
+    end
+
+    let!(:message2) do
+      create(:message, body: 'Hello', created_at: Time.zone.parse('2023-02-20 10:00'))
+    end
+
+    it 'returns display time' do
+      expect(message.display_time).to eq '09:00'
+      expect(message2.display_time).to eq 'Monday 10:00'
+    end
+  end
 end
